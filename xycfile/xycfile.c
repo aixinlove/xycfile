dce62905a8a28e0ebf1dbe48d80a5e1fe383be4e
pengyunchou <5740826@qq.com>
2014-10-31 18:40:09 +0800

    complete read write function

--- xycfile/xycfile.c
+++ xycfile/xycfile.c
@@ -18,6 +18,7 @@ int xycfile_read_header(xycfile_t *file){
     }
     return 0;
 }
+
 int xycfile_write_header(xycfile_t *file){
     size_t size=fwrite(&(file->header), sizeof(file->header), 1, file->rawfile);
     if (size!=sizeof(file->header)) {
@@ -25,10 +26,12 @@ int xycfile_write_header(xycfile_t *file){
     }
     return 0;
 }
+
 /*
  * public functions
  */
 int xycfile_open(xycfile_t *file){
+    //TODO
     file->rawfile=fopen(file->filepath, "r+");
     if (file->rawfile==NULL) {
         return -1;
@@ -41,10 +44,20 @@ int xycfile_close(xycfile_t *file){
 }
 
 int xycfile_write(xycfile_t *file,xycfile_enc_dec_block_t *block){
-    
+    xycfile_enc_dec_block_t output;
+    file->encfunc(block,&output);
+    if (fwrite(&output, sizeof(output), 1, file->rawfile)!=sizeof(output)) {
+        return -1;
+    }
     return 0;
 }
+
 int xycfile_read(xycfile_t *file,xycfile_enc_dec_block_t *block){
-    
+    xycfile_enc_dec_block_t input;
+    file->encfunc(block,&input);
+    if (fread(&input, sizeof(input), 1, file->rawfile)!=sizeof(input)) {
+        return -1;
+    }
+    file->decfunc(&input,block);
     return 0;
 }
